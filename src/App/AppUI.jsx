6b2550f1376d500cfddf9ab131f8b8f088719c3d
Donovan Rojas <rojasdrm03@gmail.com>
2024-01-09 16:18:29 -0500

    home layout

--- src/App/AppUI.jsx
+++ src/App/AppUI.jsx
@@ -2,8 +2,16 @@ import { useContext } from "react";
 import { BrowserRouter, useRoutes, Navigate } from 'react-router-dom';
 import { PortContext } from "../Context";
 import Navbar from "../Components/Navbar";
+import Home from '../Pages/Home';
 import reactLogo from '../assets/react.svg';
 
+const AppRoutes = () => {
+   
+    let routes = useRoutes ([
+      {path: '/', element: <Home />},
+    ]);
+    return routes;
+  }
 
 function AppUI() {
 
@@ -13,6 +21,7 @@ function AppUI() {
                 <div className='container'>
                     <img src={reactLogo} className="logo" alt="React logo" />
                     <Navbar />
+                    <AppRoutes />
                 </div>
             </BrowserRouter>
         </>
---
+++ src/Pages/Home/index.jsx
@@ -0,0 +1,16 @@
+import { useContext } from "react";
+import { PortContext } from "../../Context";
+import Layout from "../../Components/Layout";
+import './Home.scss';
+
+const Home = () => {
+
+  
+    return (
+      <Layout>
+        Home
+      </Layout>
+    );
+  };
+  
+  export default Home;
