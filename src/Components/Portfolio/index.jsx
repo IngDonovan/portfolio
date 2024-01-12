bbc04473e648ebf1c53b5fc47e2c0a25ec21099c
Donovan Rojas <rojasdrm03@gmail.com>
2024-01-12 11:02:53 -0500

    portfolio component

---
+++ src/Components/Portfolio/index.jsx
@@ -0,0 +1,26 @@
+import { useContext } from "react";
+import { PortContext } from "../../Context";
+import Card from "../Card";
+import "./Portfolio.sass";
+
+const Portfolio = () => {
+
+    const {
+        listProyects,
+    } = useContext(PortContext);
+
+    const renderView = () => {
+        return (
+            listProyects.map((proyect) => 
+            <Card key={proyect.id} data={proyect} />)
+        );
+    };
+
+    return (
+        <section className="proyContainer">
+            <aside>
+                {renderView()}
+            </aside>
+        </section>
+    );
+};
