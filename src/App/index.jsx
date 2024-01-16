a602f5df0d69e8221adf4a1d76e808cc616f5407
Donovan Rojas <rojasdrm03@gmail.com>
2024-01-15 19:48:51 -0500

    keyframe and better routes to img and svg

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { PortProvider } from '../Context';
 import AppUI from './AppUI';
 
-import viteLogo from '/vite.svg';
+import viteLogo from '/svg/vite.svg';
 import './App.scss';
 
 function App() {
